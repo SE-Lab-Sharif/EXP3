ee95201a1a3715b813309215b4b720d61b20b79a
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-11-09 18:39:53 +0330

    Some modifications on old tests

--- UserManagement/src/main/java/ir/selab/tdd/domain/User.java
+++ UserManagement/src/main/java/ir/selab/tdd/domain/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,4 +15,19 @@ public class User {
     private final String username;
     private final String password;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email);
+    }
 }
